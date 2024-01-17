889df15c67fb4f3b2cc4cf1653ee816f942a7373
Hugo Arcanjo <hfarcanjo@gmail.com>
2024-01-17 13:38:21 -0300

    feat: second challenge 2 solution

--- challenge02.js
+++ challenge02.js
@@ -1,14 +1,24 @@
 // Desafios 02
 
 // 1. Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+/*
 let diaDaSemana = prompt("Qual é o dia da semana?");
 if (diaDaSemana == "Domingo" || diaDaSemana == "Sábado") {
   alert("Bom fim de semana!");
 } else {
   alert("Boa semana!");
 }
+*/
 
 // 2. Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
+let numeroUsuario = prompt("Digite um número");
+if (numeroUsuario > 0) {
+  alert("Número positivo");
+} else if (numeroUsuario == 0) {
+  alert("Número zero");
+} else {
+  alert("Número negativo");
+}
 
 // 3. Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".
 
