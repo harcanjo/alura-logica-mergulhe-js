4dbab8a06c8159c501c72a1f506ac62fecaa539f
Hugo Arcanjo <hfarcanjo@gmail.com>
2024-01-17 16:22:52 -0300

    feat: sixth challenge 4 solution

--- challenge04.js
+++ challenge04.js
@@ -17,12 +17,16 @@
 // console.log(linguagem);
 
 // 5. Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha. Em seguida, realize a soma desses dois valores e armazene o resultado em uma terceira variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A soma de [valor1] e [valor2] é igual a [resultado]." no console.
-let valor1 = 13;
-let valor2 = 17;
-let resultado = valor1 + valor2;
-console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultado}.`);
+// let valor1 = 13;
+// let valor2 = 17;
+// let resultado = valor1 + valor2;
+// console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultado}.`);
 
 // 6. Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha. Em seguida, realize a subtração desses dois valores e armazene o resultado em uma terceira variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A diferença entre [valor1] e [valor2] é igual a [resultado]." no console.
+let valor1 = 35;
+let valor2 = 25;
+let resultado = valor1 - valor2;
+console.log(`A diferença entre ${valor1} e ${valor2} é igual a ${resultado}.`);
 
 // 7. Peça ao usuário para inserir sua idade com prompt. Com base na idade inserida, utilize um if para verificar se a pessoa é maior ou menor de idade, exibindo uma mensagem apropriada no console.
 
