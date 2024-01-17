5b0fd5ef3a7ea8ec317e61421d41c803d69915d9
Hugo Arcanjo <hfarcanjo@gmail.com>
2024-01-17 16:18:40 -0300

    feat: fourth challenge 4 solution

--- challenge04.js
+++ challenge04.js
@@ -9,10 +9,12 @@
 // console.log(`Olá, ${nome}!`);
 
 // 3. Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o alert para exibir a mensagem "Olá, [seu nome]!" .
-let nome = "Hugo";
-alert(`Olá, ${nome}`);
+// let nome = "Hugo";
+// alert(`Olá, ${nome}`);
 
 // 4. Utilize o prompt e faça a seguinte pergunta: Qual a linguagem de programação que você mais gosta?. Em seguida, armazene a resposta em uma variável e mostre no console do navegador.
+let linguagem = prompt("Qual a linguagem de programação que você mais gosta?");
+console.log(linguagem);
 
 // 5. Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha. Em seguida, realize a soma desses dois valores e armazene o resultado em uma terceira variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A soma de [valor1] e [valor2] é igual a [resultado]." no console.
 
