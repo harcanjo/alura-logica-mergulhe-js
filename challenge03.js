038e96e49c997e46f920a99365c2f777ac64d5ad
Hugo Arcanjo <hfarcanjo@gmail.com>
2024-01-17 15:03:13 -0300

    feat: third challenge 3 solution

--- challenge03.js
+++ challenge03.js
@@ -8,12 +8,17 @@
 // }
 
 // 2. Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
-let contador = 10;
-while (contador >= 0) {
-  console.log(`Número: ${contador}`);
-  contador--;
-}
+// let contador = 10;
+// while (contador >= 0) {
+//   console.log(`Número: ${contador}`);
+//   contador--;
+// }
 
 // 3. Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
+let contador = prompt("Digite um número");
+while (contador >= 0) {
+  console.log(`Número ${contador}`);
+  contador--;
+}
 
 // 4. Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
