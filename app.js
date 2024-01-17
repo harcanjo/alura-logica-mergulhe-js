ef6c28c6eacf419fb4ed1246c1d840e21ea63398
Hugo Arcanjo <hfarcanjo@gmail.com>
2024-01-17 14:46:56 -0300

    feat: count every try

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 2;
 console.log(numeroSecreto);
 let chute;
+let tentativas = 1;
 
 // enquanto chute não for igual ao n.s.
 while (chute != numeroSecreto) {
@@ -9,13 +10,14 @@ while (chute != numeroSecreto) {
 
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
         }else {
             alert(`O número secreto é maior que ${chute}`);        
         }
+        tentativas++;
     }
 }
 
