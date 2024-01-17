041cb56a95c80cfb996a68cbb7a0de816e3184c6
Hugo Arcanjo <hfarcanjo@gmail.com>
2024-01-17 16:17:29 -0300

    feat: third challenge 4 solution

--- challenge04.js
+++ challenge04.js
@@ -5,10 +5,12 @@
 // console.log(`Boas vindas ${nome}`);
 
 // 2. Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o console.log para exibir a mensagem "Olá, [seu nome]!" no console do navegador.
-let nome = "Hugo";
-console.log(`Olá, ${nome}!`);
+// let nome = "Hugo";
+// console.log(`Olá, ${nome}!`);
 
 // 3. Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o alert para exibir a mensagem "Olá, [seu nome]!" .
+let nome = "Hugo";
+alert(`Olá, ${nome}`);
 
 // 4. Utilize o prompt e faça a seguinte pergunta: Qual a linguagem de programação que você mais gosta?. Em seguida, armazene a resposta em uma variável e mostre no console do navegador.
 
